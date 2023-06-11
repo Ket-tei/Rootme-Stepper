196133014ae8ae35ded6439c90e426d18be733f3
Log_s <leo951206@gmail.com>
2023-06-12 00:23:11 +0200

    Rethinking combination computation + arg parsing

--- rootmeStepper.py
+++ rootmeStepper.py
@@ -1,141 +1,119 @@
-## === Library imports === ###
-import requests
-import sys
-from bs4 import BeautifulSoup
 from itertools import combinations, product
+from bs4 import BeautifulSoup
+import argparse
+import requests
+from colorama import init as colorama_init, Fore, Style
 
 
-## === Variable decleration === ##
-URL = ["https://www.root-me.org/", "?inc=score&lang=en"]
+CATEGORIES = ['steganography', 'cryptanalysis', 'forensic', 'programming', 'cracking', 'realist', 'web-server', 'app-system', 'app-script', 'web-client', 'network']
 
-
-## === Functions === ##
-def display_welcome():
-	print()
-	print(" ----------------------------------")
-	print("| ==== Rootme Step Calculator ==== |")
-	print(" ---------------------- By Log_s --")
-	print()
-
-def display_results(pseudo, goal, points, possibilities):			
-	print(f" - User   : {pseudo}")
-	print(f" - Points : {points}")
-	print(f" - Goal   : {goal}")
-	print()
-	if (len(possibilities) > 0):
-		print(f"Found {len(possibilities)} possibilities :")
-		print()
-		for possibility in possibilities:
-			print(possibility)
-	else:
-		print("Sorry, but there is no way of reaching that goal with currently available challenges")
-		print("You'll have to skip that goal, or wait for new challenges to be released")
-
-def display_error(msg, show_help):
-	print("[!] An error has occured :")
-	print(f"\t{msg}")
-	print()
-	if show_help:
-		display_help()
-	exit(0)
-
-def display_help():
-	print("Usage : python3 rootmeStepper.py <username> <goal> [<depth>]")
-	print("\tusername : The user you want to search for. Make sure he is unique (use the username that appear in the URL when checking his profile")
-	print("\tgoal : The number you want to reach (example : 8000)")
-	print("\tdepth : The maximum amount of challenges to reach the goal. Default is 3. If you choose more than 4, the calculation time may be extensive")
-	print()
-	print("Examples :")
-	print("\tpython3 rootmeStepper.py Log_s 7000 3")
-	print("\tpython3 rootmeStepper.py Log_s 8000")
-
-def parse_args():
-	if len(sys.argv) == 3:
-		pseudo = sys.argv[1]
-		try:
-			goal = int(sys.argv[2])
-		except:
-			display_error("Goal argument must be a Integer", False)
-		return pseudo, goal, 3
-	elif len(sys.argv) == 4:
-		pseudo = sys.argv[1]
-		try:
-			goal = int(sys.argv[2])
-		except:
-			display_error("Goal argument must be a Integer", False)
-		try:
-			depth = int(sys.argv[3])
-		except:
-			display_error("Depth argument must be a Integer", False)
-		return pseudo, goal, depth
-	else:
-		display_error("Wrong number of arguments", True)
 
 def get_page(pseudo):
-	global URL
-	s = requests.Session()
-	r = s.get(URL[0] + pseudo + URL[1])
-	if r.status_code == 200:
-		return r.text
-	else:
-		display_error("Couldn't find user", False)
+    s = requests.Session()
+    r = s.get(f"https://www.root-me.org/{pseudo}?inc=score&lang=en")
+    if r.status_code == 200:
+        return BeautifulSoup(r.text, 'html.parser')
+    else:
+        print(f"{Fore.RED}[!]{Style.RESET_ALL} Invalid username")
+        exit()
+
 
 def get_challenges(page):
-	soup = BeautifulSoup(page, 'html.parser')
-	links = soup.find_all("a", class_="rouge")
+	links = page.find_all("a", class_="rouge")
 	todos = []
 	for link in links:
-		parsed = str(link).split('"')
-		category = parsed[3].split("/")[2]
-		name = parsed[6].split("\xa0")[1].split("</a>")[0]
-		points = parsed[5].split()[0]
+		category = link.get("href").split("/")[2].lower()
+		name = link.text.strip().replace("x\xa0", "")
+		points = int(link.get("title").split()[0])
 		todos.append( (category, name, points) )
 	return todos
 
+
 def get_points(page):
-	soup = BeautifulSoup(page, 'html.parser')
-	points = int(soup.find_all("h3")[4].text.strip())
+	points = int(page.find_all("h3")[5].text)
 	return points
 
-def get_combinations(challenges, goal, depth):
-	numbers = [int(challenge[2]) for challenge in challenges]
-	final = []
-	for n in range(depth+1):
-		tmp = [comb for comb in combinations(numbers, n) if sum(comb) == goal]
-		final += tmp
-	final = [t for t in (set(tuple(i) for i in final))]
-	filtered = list(set( [ tuple(sorted(i)) for i in final]))
-	filtered.sort(key=lambda t: len(t))
-	return filtered
 
-def get_possibilities(challenges, combinations):
-	possibilities = []
-	for i, combination in enumerate(combinations):
-		possibilities.append([])
-		for j, element in enumerate(combination):
-			possibilities[i].append([])
-			for challenge in challenges:
-				if int(challenge[2]) == int(element):
-					possibilities[i][j].append(challenge)
-	final = []
-	for possibility in possibilities:
-		for combination in list(product(*possibility)):
-			current = ""
-			for chall in combination:
-				current += f"\t{chall[2]} : {chall[0]}/{chall[1]} \n"
-			final.append(current)
-	return final
+def filter_by_categories(challenges, add_categories, remove_categories):
+    global CATEGORIES
+    if add_categories == None and remove_categories == None:
+        return challenges
+    
+    c = []
+    if add_categories == None:
+        c = [e for e in CATEGORIES if e not in remove_categories]
+    elif remove_categories == None:
+        c = add_categories
 
-## === Main Program === ###
+    return [challenge for challenge in challenges if challenge[0] in c]
+    
+
+def parse_categories(raw):
+	# add mechanisme to recognise the intended category and fix it
+    if raw == None:
+        return None
+    return list(map(lambda x: x.strip().lower(), raw.split(",")))
+
+
+def compute_combinations(challenges, points, goal, depth):
+    valid_combinations = []
+    find_combinations(challenges, points, goal, depth, [], valid_combinations, set())
+    valid_combinations.sort(key=lambda x: sum(challenge[2] for challenge in x))
+    return valid_combinations
+
+
+def find_combinations(challenges, points, goal, depth, current_combination, valid_combinations, used_challenges):
+    if points == goal:
+        sorted_combination = sorted(current_combination, key=lambda x: x[2])
+        if sorted_combination not in valid_combinations:
+            valid_combinations.append(sorted_combination)
+    elif points < goal and depth > 0:
+        for challenge in challenges:
+            category, name, point_value = challenge
+            if challenge not in used_challenges:
+                new_points = points + point_value
+                new_depth = depth - 1
+                new_combination = current_combination + [challenge]
+                new_used_challenges = used_challenges.copy()
+                new_used_challenges.add(challenge)
+                find_combinations(challenges, new_points, goal, new_depth, new_combination, valid_combinations, new_used_challenges)
+                find_combinations(challenges, points, goal, new_depth, current_combination, valid_combinations, used_challenges)
+
+def display_results(username, points, goal, combinations):
+    pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Offers rootme challenges combinations to reach a goal')
+    parser.add_argument('username', type=str, help='The rootme username. You can find it in your profile\'s URL')
+    parser.add_argument('goal', type=int, help='The goal to reach')
+    parser.add_argument('-d', '--depth', type=int, metavar='', default=3, help='The maximum number of challenges to combine. Default is 3, higher than 4 may take a long time')
+    category_group = parser.add_mutually_exclusive_group(required=False)
+    category_group.add_argument('-a', '--add-categories', type=str, metavar='', default=None, help='The categories to include, separated by commas. Default is all categories')
+    category_group.add_argument('-e', '--exclude-categories', type=str, metavar='',default=None, help='The categories to exclude, separated by commas. Default is all categories')
+    args = parser.parse_args()
+    return args.username, args.goal, args.depth, parse_categories(args.add_categories), parse_categories(args.exclude_categories)
+
+
 def main():
-	pseudo, goal, depth = parse_args()
-	display_welcome()
-	page = get_page(pseudo)
-	challenges = get_challenges(page)
-	points = get_points(page)
-	combinations = get_combinations(challenges, goal - points, depth)
-	possibilities = get_possibilities(challenges, combinations)
-	display_results(pseudo, goal, points, possibilities)
+    colorama_init()
+
+    username, goal, depth, add_categories, exclude_categories = parse_args()
+
+    page = get_page(username)
+    challenges = get_challenges(page)
+    points = get_points(page)
+
+    if goal <= points:
+        print(f"{Fore.RED}[!]{Style.RESET_ALL} Your {goal} goal is greater or equal than your current {points} score") # TODO
+        exit()
+
+    challenges = filter_by_categories(challenges, add_categories, exclude_categories)
+
+    cmb = compute_combinations(challenges, points, goal, depth)
+    
+    display_results(username, points, goal, cmb)
+
 
 if __name__ == "__main__":
 	main()
